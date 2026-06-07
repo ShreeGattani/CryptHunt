75752b4c3131834febb5982402cb2ed145abd76d
Naahvya <46841818+navya2006@users.noreply.github.com>
2026-06-07 13:30:50 +0530

    right backlink

---
+++ app/level/right/page.tsx
@@ -0,0 +1,18 @@
+import "../backlink.css";
+
+export default function JackNylasPage() {
+  return (
+    <>
+      <main className="creepy-page">
+        <section className="creepy-card">
+            <p>You may have got it right, but what happened with Nikki was not</p>
+          <img
+            src="/images/material/onewish.png"
+            alt="Medical Report"
+            style={{ width: "100%", display: "block" }}
+          />
+        </section>
+      </main>
+    </>
+  );
+}
